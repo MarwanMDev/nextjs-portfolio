dd0f324c64408f47ef05d5551955ea84426cf875
Marwan Mostafa <Marwan.morsi@outlook.com>
2023-01-08 21:16:20 +0200

    Updating UI

--- pages/index.js
+++ pages/index.js
@@ -83,7 +83,7 @@ export default function Home() {
         </section>
         <section>
           <div>
-            <h3 className="text-3xl py-1 dark:text-white">
+            <h3 className="text-center text-3xl font-bold mb-10 py-1 dark:text-white">
               Services I offer
             </h3>
             <p className="text-md py-2 leading-8 text-gray-800 dark:text-gray-200">
